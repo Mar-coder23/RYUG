0d689c668a0486bbc06d8666ae9f6f79f8a269c9
Mar-coder23 <148150802+Mar-coder23@users.noreply.github.com>
2025-11-30 23:31:28 +0000

    fixed

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ loginBtn.addEventListener('click', () => {
     const correctPassword = "RYUG1234";
 
     if (password.value === correctPassword) {
-    window.location.href = "../homePage/home.html";
+    window.location.href = "homePage/home.html";
 
     } else {
     alert("Incorrect password. Try again.");
